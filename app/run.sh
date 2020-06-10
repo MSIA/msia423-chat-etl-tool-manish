8c853f4aab2fda889327c41d8f25b6da4812b64f
manish2020-iitr <manishkumar2020@u.northwestern.edu>
2020-06-10 11:47:09 -0500

    final

---
+++ app/run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#############################################################################################
+#                                                                                           #
+#Written by : Manish Kumar                                                                  #
+#                                                                                           #
+#Script that accepts argument wether copy or db and correspondingly runs the python files   #
+#                                                                                           #
+#############################################################################################
+
+val="$(grep 'loc_database' config/parameter.yaml)"
+val2="$(cut -d':' -f2 <<<$val)"
+
+if [ $1 = 'copy' ];
+
+then
+        python3 src/copy_to_s3.py
+
+<<<<<<< 65334ba81790dcc863ba55086bfd204ae9b61609:app/run.sh
+else
+        python3 src/rds_db.py
+=======
+elif [ $1 = 'db' ];
+
+then
+
+	python3 src/rds_db.py
+>>>>>>> final code:app/run.sh
+
+elif [ $1 = 'features' ];
+
+then
+
+    python3 src/transform_data.py
+
+elif [ $1 = 'model' ];
+
+then
+
+    python3 src/model.py
+
+elif [ $1 == 'all' ];
+
+then
+    python3 src/copy_to_s3.py
+    python3 src/rds_db.py
+    python3 src/transform_data.py
+    python3 src/model.py
+
+elif [ $1 = 'test' ];
+
+then
+    python3 -m pytest test/test.py
+fi
---
+++ app/run_docker.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+###########################################################################################################
+#                                                                                                         #
+# Written by - Manish Kumar                                                                               #
+#                                                                                                         #
+# This script runs the docker for copying file to s3 or creating a database locally or                    #
+# or on AWS RDS. It takes an argument as 'db','transform' or 'copy'. If the input is 'db'                 #
+# it checks parameter.yaml for loc_database. If the value is local it directly runs the                   #
+# rds_db.py else runs the docker                                                                          #
+#                                                                                                         #
+###########################################################################################################
+
+
+
+val="$(grep 'loc_database' config/parameter.yaml)"
+val2="$(cut -d':' -f2 <<<$val)"
+
+if [ $1 = 'db' ] && [ $val2 = 'local' ];
+then
+    python src/rds_db.py
+
+else
+
+    docker run -v "$(pwd)":"$(pwd)" -it \
+    --env aws_access_key_id \
+    --env aws_secret_access_key \
+    --env MYSQL_HOST \
+    --env MYSQL_PORT \
+    --env MYSQL_USER \
+    --env MYSQL_PASSWORD \
+    --env DATABASE_NAME \
+<<<<<<< 65334ba81790dcc863ba55086bfd204ae9b61609:app/run_docker.sh
+    s3_rds app/run.sh $1
+=======
+    s3_rds app/run.sh $1 
+>>>>>>> final code:app/run_docker.sh
+
+fi
